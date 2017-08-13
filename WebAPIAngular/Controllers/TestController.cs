c675b4aecde28b9baf62db942fb6d5929ec8952a
tejaswini24 <ktejaswini24@gmail.com>
2017-08-13 17:38:16 -0400

    Added Test Controller

---
+++ WebAPIAngular/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIAngular.Controllers
+{
+    public class TestController : ApiController
+    {
+        public List<string> GetDetails()
+        {
+
+            return new List<string> { "Hello", "First Program"};
+        }
+    }
+}
